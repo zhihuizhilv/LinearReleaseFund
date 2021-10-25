dd35b96f8bb9cba25838d66d0b7f16a45eef6e3c
zhihuizhilv <zhihuizhilv@foxmail.com>
2021-10-25 17:47:28 +0800

    deploy script

---
+++ deploy/deploy.js
@@ -0,0 +1,80 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+// import {ethers} from "hardhat";
+// import {ZERO_ADDRESS} from "../test/utils/helpers";
+
+const hre = require("hardhat");
+const ethers = hre.ethers;
+const deployResult = require('./deploy_result');
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    const treasury = deployer;
+    const protocolAdmin = deployer;
+    console.log("network:", hre.network.name);
+    console.log("deployer:", deployer.address)
+    console.log("deployer balance:", ethers.utils.formatEther((await deployer.getBalance())));
+
+    await deployResult.load();
+
+    let HarvestFactory = await ethers.getContractFactory("Harvest");
+    let DMT = "0x57A7BcdfAb1631ACA9d6E0f39959477182CfAe12";
+    let harvest = await HarvestFactory.deploy(DMT);
+    await harvest.deployed();
+    deployResult.writeAbi("Harvest", HarvestFactory);
+    deployResult.writeDeployedContract(
+      "harvest",
+      harvest.address,
+      "Harvest",
+      {
+        _dmtToken: DMT,
+      }
+    );
+
+    await deployResult.save();
+    console.log("deploy done.");
+
+    console.log("begin add funder.");
+    await (await harvest.addFunder("****************************", 10)).wait();
+    await (await harvest.addFunder("****************************", 10)).wait();
+    await (await harvest.addFunder("****************************", 10)).wait();
+    await (await harvest.addFunder("****************************", 10)).wait();
+    await (await harvest.addFunder("****************************", 10)).wait();
+    console.log("add funder end.");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ deploy/deploy_result.js
@@ -0,0 +1,73 @@
+const os = require("os");
+const fs = require("fs");
+const hre = require("hardhat");
+const {ethers} = require("ethers");
+const resultFileName = `${hre.config.paths.root}/deploy_results/deploy.${hre.network.name}.json`
+let data = {
+  abi: {},
+  deployedContract: {}
+};
+console.log("define result data");
+exports.getData = () => {
+  return data
+};
+
+exports.load = async function () {
+  if (fs.existsSync(resultFileName))
+    try {
+      data = JSON.parse(fs.readFileSync(resultFileName, {encoding: "UTF-8"}));
+      if (!data.abi || !data.deployedContract)
+        throw new Error("result file content error");
+      console.log("Deploy result load done");
+    } catch (e) {
+      console.log("Deploy result load error", e);
+      throw e;
+    }
+}
+
+exports.save = async function () {
+  fs.writeFileSync(resultFileName, JSON.stringify(data, null, 2), {encoding: "UTF-8"});
+  console.log("Deploy result: save " + resultFileName);
+}
+
+exports.writeTokens = function (tokens) {
+  for (let symbol in tokens) {
+    exports.writeToken(tokens[symbol][0], symbol, tokens[symbol][1])
+  }
+}
+
+exports.writeToken = function (address, symbol, decimals, opts) {
+  exports.writeDeployedContract("Token-" + symbol, ethers.utils.getAddress(address), "ERC20", {symbol: symbol, decimals: decimals, ...opts})
+}
+
+/**
+ * 添加合约的ABI
+ * @param name 合约名称, 唯一 (如 StakingEIP20)
+ * @param abi 合约ABI
+ */
+exports.writeAbi = function (name, abi) {
+  if (abi.interface && abi.interface.format)
+    abi = abi.interface.format("json")
+  try {
+    data.abi[name] = JSON.parse(abi);
+  } catch (e) {
+    console.error("Parse abi error : " + name + "," + abi);
+  }
+  console.log("Deploy result: write abi " + name);
+}
+
+/**
+ * 添加已创建的合约
+ * @param name 名称(如 StakingEIP20_HT_USDT 要求唯一，表示)
+ * @param address 地址
+ * @param contractName 对应 ABI 中的合约名称
+ * @param options 合约的静态参数
+ */
+exports.writeDeployedContract = function (name, address, contractName, options) {
+  for (let key in options) {
+    if (ethers.utils.isAddress(options[key]))
+      options[key] = ethers.utils.getAddress(options[key]);
+  }
+  data.deployedContract[name] = {address: ethers.utils.getAddress(address), contractName: contractName, ...options}
+  console.log("Deploy result: write deployed contract " + name + "," + address + "," + contractName);
+}
